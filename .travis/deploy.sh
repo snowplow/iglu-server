c3cd843ab2c2bcefe382a25affca1702a265f17b
Anton Parkhomenko <mailbox@chuwy.me>
2019-06-01 09:31:16 +0700

    cicd

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,19 +4,10 @@ set -e
 
 tag=$1
 
-project_version=$(sbt version -Dsbt.log.noformat=true | tail -n 1 | perl -ne 'print $1 if /(\d+\.\d+[^\r\n]*)/')
-
-cd "${TRAVIS_BUILD_DIR}"
-
-if [ "${project_version}" == "${tag}" ]; then
-    export TRAVIS_BUILD_RELEASE_TAG="${release}"
-    release-manager \
-        --config "./.travis/release.yml" \
-        --check-version \
-        --make-version \
-        --make-artifact \
-        --upload-artifact
-else
-    echo "Tag version '${tag}' doesn't match version in scala project ('${project_version}'). Aborting!"
-    exit 1
-fi
+export TRAVIS_BUILD_RELEASE_TAG="${tag}"
+release-manager \
+    --config "./.travis/release.yml" \
+    --check-version \
+    --make-version \
+    --make-artifact \
+    --upload-artifact
