b9b6425070ee01bf8b8bccf8a684a53b9a924ed1
Ian Streeter <ian@snowplowanalytics.com>
2022-04-19 13:41:24 +0100

    Bump postgresql driver to 42.3.4 (close #101)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -33,7 +33,7 @@ object Dependencies {
     val SwaggerUi  = "4.10.3"
     val Slf4j      = "1.7.36"
     val ScalaCache = "0.28.0"
-    val Postgresql = "42.2.25"
+    val Postgresql = "42.3.4"
     val Jackson    = "2.12.6.1"
 
     val Specs2     = "4.5.1"
