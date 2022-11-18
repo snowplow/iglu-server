1c60b94f91f7bab745d9ff96e0eb734bafa1d614
Benjamin Benoist <benjamin.benoist@snowplowanalytics.com>
2022-11-18 13:08:20 +0100

    Use sbt-snowplow-release and publish distroless version (close #115)

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -17,20 +17,33 @@ import sbt._
 import Keys._
 
 import sbtassembly.AssemblyPlugin.autoImport._
+
+import sbtdynver.DynVerPlugin.autoImport._
+
+import sbtbuildinfo.BuildInfoPlugin.autoImport.{BuildInfoKey, buildInfoKeys, buildInfoPackage}
+
 import com.typesafe.sbt.packager.Keys.maintainer
 import com.typesafe.sbt.packager.linux.LinuxPlugin.autoImport._
 import com.typesafe.sbt.packager.docker.DockerPlugin.autoImport._
 
 object BuildSettings {
 
+  lazy val projectSettings = Seq(
+    organization := "com.snowplowanalytics",
+    name := "iglu-server",
+    scalaVersion := "2.12.9",
+    licenses += ("Apache-2.0", url("http://www.apache.org/licenses/LICENSE-2.0.html"))
+  )
+
+  lazy val buildInfoSettings = Seq(
+    buildInfoKeys := Seq[BuildInfoKey](
+      organization, name, version,
+      BuildInfoKey.constant("SwaggerUI", Dependencies.V.SwaggerUi)    // Necessary for StaticService
+    ),
+    buildInfoPackage := "com.snowplowanalytics.iglu.server.generated"
+  )
+
   lazy val dockerSettings = Seq(
-    dockerBaseImage := "eclipse-temurin:11-jre-focal",
-    Docker / maintainer := "Snowplow Analytics Ltd. <support@snowplowanalytics.com>",
-    dockerUpdateLatest := true,
-    dockerRepository := Some("snowplow"),
-    Docker / daemonUserUid := None,
-    Docker / daemonUser := "daemon",
-    Docker / defaultLinuxInstallLocation := "/opt/snowplow",
     dockerCmd := Seq("--help")
   )
 
@@ -43,5 +56,68 @@ object BuildSettings {
         val oldStrategy = (assembly / assemblyMergeStrategy).value
         oldStrategy(x)
     }
+  )
+
+  lazy val dynVerSettings = Seq(
+    ThisBuild / dynverVTagPrefix := false, // Otherwise git tags required to have v-prefix
+    ThisBuild / dynverSeparator := "-" // to be compatible with docker
+  )
+
+  lazy val testsSettings = Seq(
+    Global / cancelable := true,
+    Test / parallelExecution := false // Because we have several specs that use the database
+  )
+
+  lazy val compilerSettings = Seq(
+    javacOptions := Seq("-source", "11", "-target", "11"),
+    addCompilerPlugin("org.spire-math" %% "kind-projector" % "0.9.9"),
+    Compile / console / scalacOptions --= Seq("-Ywarn-unused:imports", "-Xfatal-warnings"),
+    scalacOptions ++= Seq(
+      "-deprecation",                      // Emit warning and location for usages of deprecated APIs.
+      "-encoding", "utf-8",                // Specify character encoding used by source files.
+      "-explaintypes",                     // Explain type errors in more detail.
+      "-feature",                          // Emit warning and location for usages of features that should be imported explicitly.
+      "-language:existentials",            // Existential types (besides wildcard types) can be written and inferred
+      "-language:experimental.macros",     // Allow macro definition (besides implementation and application)
+      "-language:higherKinds",             // Allow higher-kinded types
+      "-language:implicitConversions",     // Allow definition of implicit functions called views
+      "-unchecked",                        // Enable additional warnings where generated code depends on assumptions.
+      "-Xcheckinit",                       // Wrap field accessors to throw an exception on uninitialized access.
+      "-Xfatal-warnings",                  // Fail the compilation if there are any warnings.
+      "-Xfuture",                          // Turn on future language features.
+      "-Xlint:adapted-args",               // Warn if an argument list is modified to match the receiver.
+      "-Xlint:by-name-right-associative",  // By-name parameter of right associative operator.
+      "-Xlint:constant",                   // Evaluation of a constant arithmetic expression results in an error.
+      "-Xlint:delayedinit-select",         // Selecting member of DelayedInit.
+      "-Xlint:doc-detached",               // A Scaladoc comment appears to be detached from its element.
+      "-Xlint:inaccessible",               // Warn about inaccessible types in method signatures.
+      "-Xlint:infer-any",                  // Warn when a type argument is inferred to be `Any`.
+      "-Xlint:missing-interpolator",       // A string literal appears to be missing an interpolator id.
+      "-Xlint:nullary-override",           // Warn when non-nullary `def f()' overrides nullary `def f'.
+      "-Xlint:nullary-unit",               // Warn when nullary methods return Unit.
+      "-Xlint:option-implicit",            // Option.apply used implicit view.
+      "-Xlint:package-object-classes",     // Class or object defined in package object.
+      "-Xlint:poly-implicit-overload",     // Parameterized overloaded implicit methods are not visible as view bounds.
+      "-Xlint:private-shadow",             // A private field (or class parameter) shadows a superclass field.
+      "-Xlint:stars-align",                // Pattern sequence wildcard must align with sequence component.
+      "-Xlint:type-parameter-shadow",      // A local type parameter shadows a type already in scope.
+      "-Xlint:unsound-match",              // Pattern match may not be typesafe.
+      "-Yno-adapted-args",                 // Do not adapt an argument list (either by inserting () or creating a tuple) to match the receiver.
+      "-Ypartial-unification",             // Enable partial unification in type constructor inference
+      "-Ywarn-dead-code",                  // Warn when dead code is identified.
+      "-Ywarn-extra-implicit",             // Warn when more than one implicit parameter section is defined.
+      "-Ywarn-inaccessible",               // Warn about inaccessible types in method signatures.
+      "-Ywarn-infer-any",                  // Warn when a type argument is inferred to be `Any`.
+      "-Ywarn-nullary-override",           // Warn when non-nullary `def f()' overrides nullary `def f'.
+      "-Ywarn-nullary-unit",               // Warn when nullary methods return Unit.
+      "-Ywarn-numeric-widen",              // Warn when numerics are widened.
+      "-Ywarn-unused:implicits",           // Warn if an implicit parameter is unused.
+      "-Ywarn-unused:imports",             // Warn if an import selector is not referenced.
+      "-Ywarn-unused:locals",              // Warn if a local definition is unused.
+      "-Ywarn-unused:params",              // Warn if a value parameter is unused.
+      "-Ywarn-unused:patvars",             // Warn if a variable bound in a pattern is unused.
+      "-Ywarn-unused:privates",            // Warn if a private member is unused.
+      "-Ywarn-value-discard"               // Warn when non-Unit expression results are unused.
+    )
   )
 }
